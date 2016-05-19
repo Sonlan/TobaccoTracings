00b24419e9fbd5d22791cd11894e6f57e54e1a71
Sonlan <1147649695@qq.com>
2016-05-19 14:34:39 +0800

    修改防伪查询显示页面

--- WebContent/super/js/antiFake.js
+++ WebContent/super/js/antiFake.js
@@ -17,26 +17,25 @@ $(document).ready(function(){
 					$(".result").attr("style","display:none");
 					$(".noData").attr("style","display:block");
 				}else{  //查询有结果
-					$(".result").attr("style","display:block");
 					$(".noData").attr("style","display:none");
+					if(data.parameter.state=="已消费"){
+						$("#warnInfo").html("该产品已被消费，消费时间为"+data.parameter.consumeTime+"，消费地点为"+data.parameter.consumeAddr);
+					}
 					if(key=="P"){//查询产品product信息
 						html="<tr><td>产品号</td><td>"+data.parameter.id+"</td><td>生产日期</td><td>"+data.parameter.pd+"</td></tr>"
 								+"<tr><td>有效期</td><td>"+data.parameter.gp+"</td><td>生产厂家</td><td>"+data.parameter.manufacturer+"</td></tr>"
 								+"<tr><td>品名</td><td>"+data.parameter.pName+"</td><td>成分</td><td>"+data.parameter.material+"</td></tr>"
-								+"<tr><td>零售价</td><td>"+data.parameter.price+"￥</td><td>销售地</td><td>"+data.parameter.targetAddr+"</td></tr>"
-								+"<tr><td>消费状态</td><td>"+data.parameter.state+"</td></tr>";
+								+"<tr><td>零售价</td><td>"+data.parameter.price+"￥</td><td>销售地</td><td>"+data.parameter.targetAddr+"</td></tr>";
 					}else if(key=="C"){//查询盒子case信息
 						html="<tr><td>产品号</td><td>"+data.parameter.caseID+"</td><td>生产日期</td><td>"+data.parameter.pd+"</td></tr>"
 						+"<tr><td>有效期</td><td>"+data.parameter.gp+"</td><td>生产厂家</td><td>"+data.parameter.manufacturer+"</td></tr>"
 						+"<tr><td>品名</td><td>"+data.parameter.pName+"</td><td>数量</td><td>"+data.parameter.amount+"包</td></tr>"
-						+"<tr><td>零售价</td><td>"+data.parameter.price+"￥</td><td>销售地</td><td>"+data.parameter.targetAddr+"</td></tr>"
-						+"<tr><td>消费状态</td><td>"+data.parameter.state+"</td></tr>";
+						+"<tr><td>零售价</td><td>"+data.parameter.price+"￥</td><td>销售地</td><td>"+data.parameter.targetAddr+"</td></tr>";
 					}else if(key=="B"){//查询盒子case信息
 						html="<tr><td>产品号</td><td>"+data.parameter.boxID+"</td><td>生产日期</td><td>"+data.parameter.pd+"</td></tr>"
 						+"<tr><td>有效期</td><td>"+data.parameter.gp+"</td><td>生产厂家</td><td>"+data.parameter.manufacturer+"</td></tr>"
 						+"<tr><td>品名</td><td>"+data.parameter.pName+"</td><td>数量</td><td>"+data.parameter.amount+"盒</td></tr>"
-						+"<tr><td>零售价</td><td>"+data.parameter.price+"￥</td><td>销售地</td><td>"+data.parameter.targetAddr+"</td></tr>"
-						+"<tr><td>消费状态</td><td>"+data.parameter.state+"</td></tr>";
+						+"<tr><td>零售价</td><td>"+data.parameter.price+"￥</td><td>销售地</td><td>"+data.parameter.targetAddr+"</td></tr>";
 					}
 					//显示
 					$("#InfoTable").html(html);
